1861f899040d6015bdb58ec841fe2471ab6a4cc5
ritesh6271 <riteshyadav839@gmail.com>
2020-11-12 14:18:51 +0530

    FacebookLoginExample

---
+++ app/src/main/java/com/example/facebookloginexample/Hash.java
@@ -0,0 +1,33 @@
+package com.example.facebookloginexample;
+
+import android.app.Application;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Hash extends Application {
+
+    public void onCreate() {
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "com.example.facebookloginexample",
+                    PackageManager.GET_SIGNATURES);
+            for (android.content.pm.Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }
+        super.onCreate();
+
+    }
+}
